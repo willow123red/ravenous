d93d3793027483854d689ce2bebfb9b4e09aabf0
willow123red <45798084+willow123red@users.noreply.github.com>
2020-06-04 17:47:38 -0600

    add term and location method handlers to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,7 +9,11 @@ class SearchBar extends React.Component {
             location : '',
             sortBy : 'best_match',
         };
-            this.sortByOptions = {
+        this.handleTermChange = this.handleTermChange.bind(this);
+        
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+
+        this.sortByOptions = {
             'Best Match': 'best_match',
             'Highest rated': 'rating',
             'Most Reviewed' : 'review_count',
@@ -26,6 +30,18 @@ class SearchBar extends React.Component {
     handleSortByChange(sortByOption) {
         this.setState({
             sortBy : sortByOption
+        });
+    }
+
+    handleTermChange(e) {
+        this.setState({
+            term : e.target.value
+        });
+    }
+
+    handleLocationChange(e) {
+        this.setState({
+            location : e.target.value
         });
     }
 
@@ -50,8 +66,8 @@ class SearchBar extends React.Component {
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+                    <input onChange={this.handleLocationChange} placeholder="Where?" />
                 </div>
                 <div className="SearchBar-submit">
                     <a href="www.#.com">Let's Go</a>
