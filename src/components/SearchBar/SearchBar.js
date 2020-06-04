f2d467d4bb04149dfcf9b46a91f023b08673bca5
willow123red <45798084+willow123red@users.noreply.github.com>
2020-06-04 17:34:32 -0600

    add 2 sorting methods and change returned list

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest rated': 'rating',
-    'Most Reviewed' : 'review_count',
-}
-
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
@@ -15,11 +9,36 @@ class SearchBar extends React.Component {
             location : '',
             sortBy : 'best_match',
         };
+            this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest rated': 'rating',
+            'Most Reviewed' : 'review_count',
+        }
     }
+    getSortByClass(sortByOption) {
+        if (this.state.sortBy === sortByOption) {
+            return 'active';
+        } else {
+            return '';
+        }  
+    }
+
+    handleSortByChange(sortByOption) {
+        this.setState({
+            sortBy : sortByOption
+        });
+    }
+
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>    
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return <li 
+                        key={sortByOptionValue}
+                        className={this.getSortByClass(sortByOptionValue)}
+                        onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+                        >
+                        {sortByOption}
+                    </li>    
         });
     }
     render() {
